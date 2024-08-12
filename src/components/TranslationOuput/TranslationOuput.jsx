a0327f6a8304f948f3c4b575a6143c2930a21723
Talal Salman Zafeer <mustangcreator@gmail.com>
2024-08-12 20:58:03 +0300

    Design And Layout Added

--- src/components/TranslationOuput/TranslationOuput.jsx
+++ src/components/TranslationOuput/TranslationOuput.jsx
@@ -3,6 +3,7 @@ import './TranslationOuput.css';
 
 import swap from '../../assets/Horizontal_top_left_main.svg';
 import LanguageSelect from '../LanguageSelect/LanguageSelect';
+import SpeakAndCopy from '../SpeakAndCopy/SpeakAndCopy';
 
 const TranslationOuput = () => {
   return (
@@ -10,6 +11,14 @@ const TranslationOuput = () => {
         <div className='selection-container'>
           <LanguageSelect isInput={false} />
           <button className='btn-swap'><img src={swap} alt='swap' className='swapIcon' /></button>
+        </div>
+        <div className='outputResults-display'>
+          <p className='translatedResult'>
+            
+          </p>
+        </div>
+        <div className='speakAndCopy-container'>
+          <SpeakAndCopy isInput={false} />
         </div>
     </div>
   )
